423edaf6dede45c1ffad83fe8be1b4c3bcb83e01
YagoEngels <61044864+YagoEngels@users.noreply.github.com>
2020-11-08 18:11:29 +0100

    de layout van de add en lijst is aangepast zodat ze beter meegaat met de volledige app

--- app/src/main/java/com/example/chewie_week_school/ObjectenLijst.java
+++ app/src/main/java/com/example/chewie_week_school/ObjectenLijst.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,6 +27,9 @@ public class ObjectenLijst extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objecten_lijst);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         databaseObjecten = new DatabaseObjecten(this);
         listView = findViewById(R.id.listviewObjecten);
